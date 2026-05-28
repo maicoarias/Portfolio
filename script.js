6f54860caefa9945ad24304e4bd0d6698117da0b
maicoarias <maicoarias56@gmail.com>
2026-05-28 19:39:30 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-// SCROLL HEADER
-
-window.addEventListener("scroll", () => {
-
-  const header = document.querySelector(".header");
-
-  if(window.scrollY > 50){
-    header.classList.add("active");
-  }
-  else{
-    header.classList.remove("active");
-  }
-
-});
-
-// REVEAL ANIMATION
-
 const revealElements = document.querySelectorAll(
-  ".skill-card, .project-card, .about-card, .timeline-item, .contact-card"
+  ".project-card, .skills-box, .contact-card, .timeline-item, .service-card, .extra-card"
 );
 
-window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", revealOnScroll);
 
-function reveal(){
+function revealOnScroll(){
 
   const triggerBottom = window.innerHeight * 0.85;
 
@@ -37,4 +20,4 @@ function reveal(){
 
 }
 
-reveal();
+revealOnScroll();
